ba1ba8ceba324707090264b224f1b86673c43a8d
112360228 <t112360228@ntut.org.tw>
2025-10-18 00:19:05 +0800

    java_HTML_update
    HTML網頁美工，帶增加更多，解決bug和新增新增成功訊息

--- Material Management System/mms/src/main/java/com/morris/mms/mms/MmsApplication.java
+++ Material Management System/mms/src/main/java/com/morris/mms/mms/MmsApplication.java
@@ -1,4 +1,4 @@
-package com.morris.mms;
+package com.morris.mms.mms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Material Management System/mms/src/test/java/com/morris/mms/mms/MmsApplicationTests.java
+++ Material Management System/mms/src/test/java/com/morris/mms/mms/MmsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.morris.mms;
+package com.morris.mms.mms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
